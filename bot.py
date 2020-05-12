557d83550f7873869c8228324d32f0fd273ed6ac
thisisfunt <64325631+thisisfunt@users.noreply.github.com>
2020-05-12 15:39:49 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+import random
+
+maps = ["Mirage", "Overpass", "Inferno", "Dust II"]
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.split(" ")[0] == "bot":
+            if message.content.split(" ")[1] == "map":
+                map = maps[random.randint(0, len(maps) - 1)]
+                await message.channel.send(map)
+            
+client = MyClient()
+client.run('NzA5NDMxMjQyMjY4OTk5ODEx.Xrlzeg.exVOZhzXQApdrApVZ_Vg7RPLi_U')
