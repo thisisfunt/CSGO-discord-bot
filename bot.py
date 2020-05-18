6990c5399b78c9d6fa9d2d80b97381d2317499cd
thisisfunt <64325631+thisisfunt@users.noreply.github.com>
2020-05-18 20:58:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,9 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-hltv = "https://www.hltv.org/matches"
+hltvstat = "https://www.hltv.org/stats"
+hltvlife = "https://www.hltv.org/matches"
+hltvplayers = "https://www.hltv.org/stats/players?startDate=2019-05-18&endDate=2020-05-18&rankingFilter=Top20"
 user = {"UserAgent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"}
 maps = ["Mirage", "Overpass", "Inferno", "Dust II"]
 
@@ -20,13 +22,21 @@ class MyClient(discord.Client):
                 map = maps[random.randint(0, len(maps) - 1)]
                 await message.channel.send(map)
             elif message.content.split(" ")[1] == "life" and len(message.content.split(" ")) == 2:
-                full_page = requests.get(hltv, user)
+                full_page = requests.get(hltvlife, user)
                 soup = BeautifulSoup(full_page.content, "html.parser")
                 info = soup.findAll("span", {"class":"team-name"})
                 fight1 = info[0].text + " vs " + info[1].text
                 fight2 = info[2].text + " vs " + info[3].text
-                await message.channel.send("Матчи в эфире:" + "\n" + fight1 + "\n" + fight2 + "\n" + hltv)
-
+                await message.channel.send("Матчи в эфире:" + "\n" + fight1 + "\n" + fight2 + "\n" + hltvlife)
+            elif message.content.split(" ")[1] == "players" and len(message.content.split(" ")) == 2:
+                full_page = requests.get(hltvstat, user)
+                soup = BeautifulSoup(full_page.content, "html.parser")
+                playerinfo = soup.findAll("a", {"class":"name"})
+                kdinfo = soup.findAll("span", {"class":"bold"})
+                playertext = "Топ игроков:\n"
+                for i in range(0, len(playerinfo)):
+                    playertext += playerinfo[i].text + " - " + kdinfo[i * 2].text + "\n"
+                await message.channel.send(playertext + "\n" + hltvstat)
             else:
                 await message.channel.send("Я твоя не понимать!")
 client = MyClient()
