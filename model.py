f15efaeea5dc5b1d9449484997095b3ef75838b0
thisisfunt <salnikov17a@gmail.com>
2021-02-19 21:37:31 +0300

    Разделено отображение и парсинг, замена html парсера на модуль, переписана значительная часть логики обработки запросов и их обработки, повышена читабильность кода

---
+++ model.py
@@ -0,0 +1,37 @@
+import random
+from HLTV import *
+
+hltvteams = "https://www.hltv.org/stats/teams"
+hltvlife = "https://www.hltv.org/matches"
+hltvplayers = "https://www.hltv.org/stats/players?startDate=2019-05-18&endDate=2020-05-18&rankingFilter=Top20"
+user = {"UserAgent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"}
+maps = ["Mirage", "Overpass", "Inferno", "Dust II"]
+
+def helped_message():
+    return "helped massage"
+
+def random_choice(choiseList : list):
+    return "Случайный выбор : " + random.choice(choiseList)
+
+def best_match():
+    result = "Матчи : \n"
+    for match in get_live_matches():
+        result += match['teams'][0] + " -vs- " + match['teams'][0] + "\n"
+    result += hltvlife
+    return result
+
+def best_teams():
+    best_10_teams = get_top_teams()[:10]
+    result = "Лучшие команды:\n"
+    for team_id in range(len(best_10_teams)):
+        result += str(team_id + 1) + ". " + best_10_teams[team_id] + "\n"
+    result += hltvteams
+    return result
+
+def best_players():
+    top_10_players = get_best_players()[:10]
+    result = "Лучшие игроки:\n"
+    for player_data_id in range(len(top_10_players)):
+        result += str(player_data_id + 1) + ". " + top_10_players[player_data_id]['player'] + " - " + top_10_players[player_data_id]['team'] + " - " + top_10_players[player_data_id]['rating'] + "\n"
+    result += hltvplayers
+    return result
---
+++ view.py
@@ -0,0 +1,38 @@
+import discord
+import random
+import requests
+from bs4 import BeautifulSoup
+import os
+import model as model
+
+hltvstat = "https://www.hltv.org/stats"
+hltvlife = "https://www.hltv.org/matches"
+hltvplayers = "https://www.hltv.org/stats/players?startDate=2019-05-18&endDate=2020-05-18&rankingFilter=Top20"
+user = {"UserAgent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"}
+maps = ["Mirage", "Overpass", "Inferno", "Dust II"]
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+            if message.author == self.user:
+                return
+            splited_message = message.content.split(" ")
+            if splited_message[0] == "bot":
+                if splited_message[1] == "help" and len(splited_message) == 2:
+                    await message.channel.send(model.helped_message())
+                elif splited_message[1] == "random":
+                    await message.channel.send(model.random_choice(splited_message[2:]))
+                elif splited_message[1] == "matches" and len(splited_message) == 2:
+                    await message.channel.send(model.best_match())
+                elif splited_message[1] == "teams" and len(splited_message) == 2:
+                    await message.channel.send(model.best_teams())
+                elif splited_message[1] == "players" and len(splited_message) == 3:
+                    await message.channel.send(model.best_players())
+
+client = MyClient()
+
+#token = os.environ.get("token_side")
+token = "NzA5NDMxMjQyMjY4OTk5ODEx.Xs_yNg.zWChDkYXRB9NCTCffpdJeKkSsvE"
+client.run(str(token))
